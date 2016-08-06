99f89a219224b28181c26f5662d4a9b8ac49a540
Kavita0910 <kavita.gurav9@gmail.com>
2016-08-06 16:37:21 +0530

    Maven

---
+++ src/qsp/DemoA.java
@@ -0,0 +1,17 @@
+package qsp;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	
+	@Test
+	public void testA()
+	{
+		System.setProperty("webdriver.gecko.driver", ".//exes//geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+	}
+
+}
